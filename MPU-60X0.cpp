9c318fa1f1369a6064e8527cd5c2d9d803a947b2
chisl <info@chisl.io>
2017-12-27 18:15:00 +0100

    Initial commit

---
+++ MPU-60X0.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        MPU-60X0
+ * description: MPU-6050 Six-Axis (Gyro + Accelerometer) MEMS Motion Tracking Devices
+ * manuf:       InvenseSense
+ * version:     0.1
+ * url:         https://store.invensense.com/datasheets/invensense/MPU-6050_DataSheet_V3%204.pdf
+ * date:        2016-08-01
+ * author       https://chisl.io/
+ * file:        MPU-60X0.cpp
+ */
+
+#include "MPU-60X0.hpp"
+
